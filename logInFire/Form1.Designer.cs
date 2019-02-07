34a98e15e79f1967288192e39a320cf6092cbd74
akar imdad <akar.imdad1995@gmail.com>
2019-02-08 00:17:12 +0300

    test

--- logInFire/Form1.Designer.cs
+++ logInFire/Form1.Designer.cs
@@ -28,13 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.ServiceProcess.ServiceController serviceController1;
     }
 }
 
